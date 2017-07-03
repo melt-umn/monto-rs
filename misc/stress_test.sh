6c7e6887a8665c8b472b8dfcec5a6827f35bd1c5
Nathaniel Ringo <remexre@gmail.com>
2017-07-03 12:11:12 -0500

    Adds stress test script.

---
+++ misc/stress_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+function report() {
+	vegeta report -inputs ../report/stress_test.bin -output "../report/stress_test.${2}" -reporter "${1}"
+}
+
+cd "$(dirname "${BASH_SOURCE[0]}")/data"
+
+command -v vegeta >/dev/null || go get -u github.com/tsenart/vegeta
+
+mkdir -p "../report"
+echo "Stress testing for 60 seconds..."
+vegeta attack -duration 60s -rate ${1:-1000} -targets stress_test.targets -output ../report/stress_test.bin
+
+echo "Creating reports..."
+report text txt
+report json json
+report plot html
+report 'hist[0,2ms,4ms,6ms,8ms,10ms]' hist.txt
