639e79efdd196feff7aafd8ac698f9c90dde64a0
Nathaniel Ringo <remexre@gmail.com>
2017-08-29 14:35:28 -0500

    Hotfix for macro borking.

--- src/macros.rs
+++ src/macros.rs
@@ -4,8 +4,18 @@
 /// # #[macro_use] extern crate monto;
 /// simple_service_provider! {
 ///     name = Example;
-///     product = errors;
-///     language = text;
+///     product = "errors";
+///     language = "text";
+///     (path, input) => {
+///         unimplemented!();
+///     }
+/// }
+///
+/// simple_service_provider! {
+///     name = OtherExample;
+///     // Both of these are "custom".
+///     product = "edu.umn.cs.melt.custom_product";
+///     language = "ableC";
 ///     (path, input) => {
 ///         unimplemented!();
 ///     }
@@ -16,46 +26,18 @@
 macro_rules! simple_service_provider {
     (
         name = $name:ident;
-        product = $product:ident;
-        language = $lang:ident;
+        product = $product:expr;
+        language = $lang:expr;
         ($path:ident, $input:ident) => $body:block
     ) => {
         pub struct $name;
         impl $crate::service::ServiceProvider for $name {
             fn descriptor(&self) -> $crate::common::messages::ProductDescriptor {
-                let name = __product_name!($product);
-                let language = __product_lang!($lang);
+                let name = $product.parse().expect("Invalid product name from macro");
+                let language = $lang.to_owned().into();
                 $crate::common::messages::ProductDescriptor { name, language }
             }
             fn service(&mut self, $path: &str, $input: ::std::vec::Vec<$crate::common::messages::Product>) -> ::std::result::Result<$crate::service::messages::ServiceProduct, $crate::service::messages::ServiceErrors> $body
         }
     }
-}
-
-#[macro_export]
-#[doc(hidden)]
-macro_rules! __product_name {
-    (directory) => { $crate::common::messages::ProductName::Directory };
-    (errors) => { $crate::common::messages::ProductName::Errors };
-    (highlighting) => { $crate::common::messages::ProductName::Highlighting };
-    (source) => { $crate::common::messages::ProductName::Source };
-    ($($n:ident .)+ $l:ident) => {{
-        let mut s = ::std::string::String::new();
-        $({
-            s.push_str(stringify!($n));
-            s.push('.');
-        })+
-        s.push_str(stringify!($l));
-        s.parse().expect("Invalid product name from a macro")
-    }};
-}
-
-#[macro_export]
-#[doc(hidden)]
-macro_rules! __product_lang {
-    (json) => { $crate::common::messages::Language::Json };
-    (none) => { $crate::common::messages::Language::None };
-    (text) => { $crate::common::messages::Language::Text };
-    ($i:ident) => { $crate::common::messages::Language::from(stringify!($i).to_owned()) };
-    ($e:expr) => { $crate::common::messages::Language::from($e) };
 }
